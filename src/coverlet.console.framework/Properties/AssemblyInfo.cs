8bc5b5da9d5622a042d02ce796ee67eabf4eff1f
cjewett <cjewett@alarm.com>
2020-08-12 19:34:38 -0400

    update master

--- src/coverlet.console.framework/Properties/AssemblyInfo.cs
+++ src/coverlet.console.framework/Properties/AssemblyInfo.cs
@@ -1,1 +1,5 @@
-﻿[assembly: System.Reflection.AssemblyKeyFileAttribute("coverlet.console.framework.snk")]
+﻿using System.Reflection;
+
+[assembly: AssemblyKeyFileAttribute("coverlet.console.framework.snk")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
