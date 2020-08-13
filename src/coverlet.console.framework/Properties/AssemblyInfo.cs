9561cbad0f9f3b8eca7a7915892194cae5580dbe
C J <Cjewett@users.noreply.github.com>
2020-08-12 21:51:02 -0400

    update

--- src/coverlet.console.framework/Properties/AssemblyInfo.cs
+++ src/coverlet.console.framework/Properties/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyKeyFileAttribute("coverlet.console.framework.snk")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
